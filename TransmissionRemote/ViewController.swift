ad3c56892103f68df62747f684a645b5d45466ed
sligoo <sacha.liguori@icloud.com>
2016-11-13 10:45:13 +0100

    First Commit

--- TransmissionRemote/ViewController.swift
+++ TransmissionRemote/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("I can't wait to push this project on GitHub")
     }
 
     override func didReceiveMemoryWarning() {
