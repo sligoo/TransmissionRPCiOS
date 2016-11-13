20736b67920da1f24a41dee7fb3dd8cfba0e25ef
sligoo <sacha.liguori@icloud.com>
2016-11-13 10:43:26 +0100

    Initial Commit

---
+++ TransmissionRemote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransmissionRemote
+//
+//  Created by Sacha Liguori on 13/11/2016.
+//  Copyright © 2016 Sacha Liguori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
